cba849b1b176f7b226d67daecf273f68020c4178
Keegan Given <32858528+gkeegan@users.noreply.github.com>
2018-12-07 02:12:41 +0000

    Update sms-via-email.py
    Add spaces to ends of inputs

--- sms-via-email.py
+++ sms-via-email.py
@@ -12,9 +12,9 @@ port = '587' # SMTP port
     # This section has two modes: editing the info into the .py file, or using user input in shell
     # Make whichever one you don't want to use into a comment
 # recipient = "" # who will receive the message. put this in email notation $$$$$$$$$$@txt.att.net
-recipient = input("What number do you want to send to? Include the email ending. Press enter when done.")
+recipient = input("What number do you want to send to? Include the email ending. Press enter when done. ")
 # message = "" # what will be sent
-message = input("Input your message. Pressing enter will send it.")
+message = input("Input your message. Pressing enter will send it. ")
 
 # Sending Process
 server = smtplib.SMTP(host, port)
