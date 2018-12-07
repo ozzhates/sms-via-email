b6f6c8292bfc02d458c42021e6b431d3ecf54067
gkeegan <32858528+gkeegan@users.noreply.github.com>
2018-12-06 20:02:15 -0600

    Rearrange some lines, and add more comments

--- sms-via-email.py
+++ sms-via-email.py
@@ -3,21 +3,21 @@
 import smtplib
 
 # Setup
-email_host = 'smtp.gmail.com'
-address = ''
-port = '587'
-password = ''
+address = '' # your email
+password = '' # your email password
+host = 'smtp.gmail.com' # SMTP server of your email provider
+port = '587' # SMTP port
 
 # Message and Recipient
     # This section has two modes: editing the info into the .py file, or using user input in shell
     # Make whichever one you don't want to use into a comment
 # recipient = "" # who will receive the message. put this in email notation $$$$$$$$$$@txt.att.net
-recipient = input("What number do you want to send to? Include the email ending.")
+recipient = input("What number do you want to send to? Include the email ending. Press enter when done.")
 # message = "" # what will be sent
-message = input("Message? ")
+message = input("Input your message. Pressing enter will send it.")
 
 # Sending Process
-server = smtplib.SMTP(email_host, port)
+server = smtplib.SMTP(host, port)
 server.starttls()
 server.login(address, password)
 server.sendmail(address,recipient,message)
