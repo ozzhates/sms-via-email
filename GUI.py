f9469da6d3dff0a0eeedac1f91df4bd115d53bc0
gkeegan <32858528+gkeegan@users.noreply.github.com>
2018-12-13 19:13:29 -0600

    Create GUI.py
    GUI version made with tkinter

---
+++ GUI.py
@@ -0,0 +1,44 @@
+from tkinter import *
+import smtplib
+
+root = Tk()
+
+host = 'smtp.gmail.com' # SMTP server of your email provider
+port = '587' # SMTP port
+# Functions
+def sending():
+    email = e_email.get()
+    pswd = e_pswd.get()
+    rcpt = e_rcpt.get()
+    msg = e_msg.get()
+    server = smtplib.SMTP(host, port)
+    server.starttls()
+    server.login(email, pswd)
+    server.sendmail(email,rcpt,msg)
+    server.quit()
+    e_msg.delete(0, 'end') # Clears message entry box
+
+# Design
+labelemail = Label(root, text="Email")
+e_email = Entry(root, bd =5)
+labelpswd = Label(root, text="Password")
+e_pswd = Entry(root, bd =5, show='*')
+labelrcpt = Label(root, text="Recipient")
+e_rcpt = Entry(root, bd =5)
+labelmsg = Label(root, text="Message")
+e_msg = Entry(root, bd =5)
+submit = Button(root, text ="Submit", command = sending)
+
+# Making design visible
+labelemail.grid(column=0, row=0, sticky=E)
+labelpswd.grid(column=0, row=1, sticky=E)
+labelrcpt.grid(column=0, row=2, sticky=E)
+labelmsg.grid(column=0, row=3, sticky=E)
+
+e_email.grid(column=1, row=0)
+e_pswd.grid(column=1, row=1)
+e_rcpt.grid(column=1, row=2)
+e_msg.grid(column=1, row=3)
+submit.grid(column=2, row=3)
+
+root.mainloop()
