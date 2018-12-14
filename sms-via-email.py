f6aa1fb83c094eb62e69ed8c7f628829527054de
gkeegan <32858528+gkeegan@users.noreply.github.com>
2018-12-13 19:07:35 -0600

    Update sms-via-email.py
    Shorten variables to match "GUI.py" file

--- sms-via-email.py
+++ sms-via-email.py
@@ -3,8 +3,8 @@
 import smtplib
 
 # Setup
-address = '' # your email
-password = '' # your email password
+email = '' # your email
+pswd = '' # your email password
 host = 'smtp.gmail.com' # SMTP server of your email provider
 port = '587' # SMTP port
 
@@ -12,14 +12,14 @@ port = '587' # SMTP port
     # This section has two modes: editing the info into the .py file, or using user input in shell
     # Make whichever one you don't want to use into a comment
 # recipient = "" # who will receive the message. put this in email notation $$$$$$$$$$@txt.att.net
-recipient = input("What number do you want to send to? Include the email ending. Press enter when done. ")
+rcpt = input("What number do you want to send to? Include the email ending. Press enter when done. ")
 # message = "" # what will be sent
-message = input("Input your message. Pressing enter will send it. ")
+msg = input("Input your message. Pressing enter will send it. ")
 
 # Sending Process
 server = smtplib.SMTP(host, port)
 server.starttls()
-server.login(address, password)
-server.sendmail(address,recipient,message)
+server.login(email, pswd)
+server.sendmail(email,rcpt,msg)
 print('Message Sent')
 server.quit()
