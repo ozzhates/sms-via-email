d8bf7b6f0af2e9dcd8b0ad320c813a54eadd0bbd
gkeegan <32858528+gkeegan@users.noreply.github.com>
2019-06-26 16:12:21 -0500

    Consolidate displaying with design in GUI

--- GUI.py
+++ GUI.py
@@ -19,26 +19,16 @@ def sending():
     e_msg.delete(0, 'end') # Clears message entry box
 
 # Design
-labelemail = Label(root, text="Email")
-e_email = Entry(root, bd =1)
-labelpswd = Label(root, text="Password")
-e_pswd = Entry(root, bd =1, show='*')
-labelrcpt = Label(root, text="Recipient")
-e_rcpt = Entry(root, bd =1)
-labelmsg = Label(root, text="Message")
-e_msg = Entry(root, bd =1)
-send = Button(root, text ="Send", command = sending, bd =1)
+labelemail = Label(root, text="Email").grid(column=0, row=0, sticky=E)
+e_email = Entry(root, bd =1).grid(column=1, row=0)
+labelpswd = Label(root, text="Password").grid(column=0, row=1, sticky=E)
+e_pswd = Entry(root, bd =1, show='*').grid(column=1, row=1)
+labelrcpt = Label(root, text="Recipient").grid(column=0, row=2, sticky=E)
+e_rcpt = Entry(root, bd =1).grid(column=1, row=2)
+labelmsg = Label(root, text="Message").grid(column=0, row=3, sticky=E)
+e_msg = Entry(root, bd =1).grid(column=1, row=3)
+send = Button(root, text ="Send", command = sending, bd =1).grid(column=2, row=3)
 
-# Making design visible
-labelemail.grid(column=0, row=0, sticky=E)
-labelpswd.grid(column=0, row=1, sticky=E)
-labelrcpt.grid(column=0, row=2, sticky=E)
-labelmsg.grid(column=0, row=3, sticky=E)
-
-e_email.grid(column=1, row=0)
-e_pswd.grid(column=1, row=1)
-e_rcpt.grid(column=1, row=2)
-e_msg.grid(column=1, row=3)
-send.grid(column=2, row=3)
 root.title("sms-via-email")
+
 root.mainloop()
