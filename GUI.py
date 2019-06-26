8b5885cf62293c98084e2222987f1131b5b6c67b
gkeegan <32858528+gkeegan@users.noreply.github.com>
2019-06-26 17:10:05 -0500

    fix gridding
    prior to this, gridding on variables that are needed to be used set their value to None, which interfered with operation

--- GUI.py
+++ GUI.py
@@ -20,13 +20,17 @@ def sending():
 
 # Design
 labelemail = Label(root, text="Email").grid(column=0, row=0, sticky=E)
-e_email = Entry(root, bd =1).grid(column=1, row=0)
+e_email = Entry(root, bd =1)
+e_email.grid(column=1, row=0)
 labelpswd = Label(root, text="Password").grid(column=0, row=1, sticky=E)
-e_pswd = Entry(root, bd =1, show='*').grid(column=1, row=1)
+e_pswd = Entry(root, bd =1, show='*')
+e_pswd.grid(column=1, row=1)
 labelrcpt = Label(root, text="Recipient").grid(column=0, row=2, sticky=E)
-e_rcpt = Entry(root, bd =1).grid(column=1, row=2)
+e_rcpt = Entry(root, bd =1)
+e_rcpt.grid(column=1, row=2)
 labelmsg = Label(root, text="Message").grid(column=0, row=3, sticky=E)
-e_msg = Entry(root, bd =1).grid(column=1, row=3)
+e_msg = Entry(root, bd =1)
+e_msg.grid(column=1, row=3)
 send = Button(root, text ="Send", command = sending, bd =1).grid(column=2, row=3)
 
 root.title("sms-via-email")
